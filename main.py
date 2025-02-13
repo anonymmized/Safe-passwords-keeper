db9c5f36e5602ed35be0a4959da976946c75ae6a
lx-insane <lx@insane.local>
2025-02-13 21:03:13 +0300

    004 -> Main file was created

---
+++ main.py
@@ -0,0 +1,8 @@
+import enc_dec_file
+import mover
+import time
+
+def main():
+    print("[#] This program moving your files with 'pass' in filenames to flash drive!")
+    time.sleep(3)
+    answ = input("[*] Preparation is completed\n Do you want to move your files [Y/n]: ")
