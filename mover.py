6c259e276c81d18b7e694256bdbdc4ce457df3ad
lx-insane <lx@insane.local>
2025-02-14 22:40:17 +0300

    007 -> Revealing the check of the existence in mover.py

--- mover.py
+++ mover.py
@@ -1,15 +1,26 @@
 """/Volumes/pass_keeper"""
 import os
 
+from sympy.codegen import While
+
 
 def mover(drive, filepath):        # <- сюда будет передаваться уже зашифрованный файл
     cur_dir = "/Volumes"
     folder_path = os.path.join(cur_dir, drive)
     os.system(f"mv {filepath} {folder_path}")
-
-drive = input("[?] Enter the name of the drive: ")
-filename = input("[?] Enter filename: ")
-print("[*] Moving your file on flash drive...")
-mover(drive, filename)
-print("[***] Complete!")
+while True:
+    drive = input("[?] Enter the name of the drive: ")
+    if os.path.ismount(f"/Volumes/{drive}"):
+        while True:
+            filename = input("[?] Enter filename: ")
+            if os.path.isfile(filename):
+                print("[*] Moving your file on flash drive...")
+                mover(drive, filename)
+                print("[***] Complete!")
+                break
+            else:
+                print("[!] There is no file like that, please retype...")
+        break
+    else:
+        print("[!] There is no drive like that, please retype...")
 # проверять filename на наличие в существующей директории
